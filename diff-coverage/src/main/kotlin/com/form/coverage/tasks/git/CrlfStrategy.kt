9afd4941b8d50a6e318a0b9e1cd2686bf824f4be
Sergiy Gnatyuk <surpsg2012@gmail.com>
2020-06-22 02:00:57 +0300

    fix default line separator

--- diff-coverage/src/main/kotlin/com/form/coverage/tasks/git/CrlfStrategy.kt
+++ diff-coverage/src/main/kotlin/com/form/coverage/tasks/git/CrlfStrategy.kt
@@ -1,11 +1,8 @@
 package com.form.coverage.tasks.git
 
 import org.eclipse.jgit.lib.CoreConfig
-import java.io.File
 
-const val FILE_SEPARATOR: String = "\r\n"
-
-fun getCrlf(lineSeparator: String = FILE_SEPARATOR): CoreConfig.AutoCRLF {
+fun getCrlf(lineSeparator: String = System.lineSeparator()): CoreConfig.AutoCRLF {
     return if (lineSeparator == "\r\n") {
         CoreConfig.AutoCRLF.TRUE
     } else {
