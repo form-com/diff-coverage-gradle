447baa887a52ef40ee954849f513c9b11703df5f
Sergii Gnatiuk <surpsg2012@gmail.com>
2020-08-10 22:10:07 +0300

    No package class path resolve fix (#10)
    * fix class path resolve for classes without package
    
    * remove mock server dependency
    
    * increase version
    
    Co-authored-by: Sergii Gnatiuk <sergii.gnatiuk@worldapp.com>

---
+++ jacoco-filtering-extension/src/test/kotlin/com/form/diff/ClassFileTest.kt
@@ -0,0 +1,24 @@
+package com.form.diff
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+
+class ClassFileTest: StringSpec({
+
+    "ClassFile.path should return relative path when class has no package" {
+        // setup
+        val classFile = ClassFile("Class1.java", "Class1")
+
+        // run // assert
+        classFile.path shouldBe "Class1.java"
+    }
+
+    "ClassFile.path should return relative path when class has package" {
+        // setup
+        val classFile = ClassFile("Class1.java", "com/java/test/Class1")
+
+        // run // assert
+        classFile.path shouldBe "com/java/test/Class1.java"
+    }
+})
