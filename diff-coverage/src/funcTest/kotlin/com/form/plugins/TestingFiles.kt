1a06ace02a370b515ccea8304162e50b1e8435b2
sergii.gnatiuk <sergii.gnatiuk@worldapp.com>
2021-08-20 01:46:15 +0300

    collect jacoco configuration from all projects

--- diff-coverage/src/funcTest/kotlin/com/form/plugins/TestingFiles.kt
+++ diff-coverage/src/funcTest/kotlin/com/form/plugins/TestingFiles.kt
@@ -5,20 +5,23 @@ import java.io.File
 
 inline fun <reified T> getResourceFile(filePath: String): File {
     return T::class.java.classLoader
-            .getResource(filePath)!!.file
-            .let(::File)
+        .getResource(filePath)!!.file
+        .let(::File)
 }
 
-inline fun <reified T> TemporaryFolder.copyResourceFile(fileFrom: String, fileTo: String): File {
+inline fun <reified T> TemporaryFolder.copyFileFromResources(fileFrom: String, fileTo: String): File {
     return getResourceFile<T>(fileFrom).copyTo(newFile(fileTo), true)
 }
 
-inline fun <reified T> TemporaryFolder.copyResourceDir(dirToCopy: String, destDir: String): String {
+inline fun <reified T> TemporaryFolder.copyDirFromResources(
+    dirToCopy: String,
+    destDir: String = dirToCopy
+): File {
     getResourceFile<T>(dirToCopy).copyRecursively(
         newFolder(destDir),
         true
     )
-    return root.resolve(destDir).toUnixAbsolutePath()
+    return root.resolve(destDir)
 }
 
-fun File.toUnixAbsolutePath() : String = absolutePath.replace("\\", "/")
+fun File.toUnixAbsolutePath(): String = absolutePath.replace("\\", "/")
